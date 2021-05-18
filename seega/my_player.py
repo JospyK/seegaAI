be7473ac7b1ffa99b59ffc71a70f17f2b051267d
Jospy CAA <jospy.goudalo@caabenin.org>
2021-05-18 23:37:54 +0100

    ok

--- seega/my_player.py
+++ seega/my_player.py
@@ -25,56 +25,56 @@ class AI(Player):
     def play(self, state, remain_time):
         print(f"Player {self.position} is playing.")
         #print("time remain is ", remain_time, " seconds")
-        # pprint(vars(self.position))
-        
-        
-        actions = SeegaRules.get_player_all_cases_actions(state, self.position)
-        return self.best_action(state, actions)
 
+        if state.phase == 1 :
+            return self.best_entry_position(state)
+        else: 
+            return self.best_action(state)
+                
         
     def set_score(self, new_score):
         self.score = new_score
 
+
     def update_player_infos(self, infos):
         self.in_hand = infos['in_hand']
         self.score = infos['score']
-        
+
+
     def reset_player_informations(self):
         self.in_hand = 12
         self.score = 0
 
-    def best_position(self, state, actions):
-        pass
 
-    def best_action(self, state, actions):
-        import random
-        choose = random.choice(actions)
-        move = None
+    def best_entry_position(self, state):
+        return SeegaRules.random_play(state, self.position)
+
+
+    def best_action(self, state):
+        actions = SeegaRules.get_player_all_cases_actions(state, self.position)
+        pprint(actions)
+        move = actions[0]
         evaluation = 0
-        #pprint(self.minimax(state, 8, -math.inf, math.inf, True))
-        # move, evaluation = self.minimax(state, 8, -math.inf, math.inf, True)
+        move, evaluation = self.minimax(state, 20, -math.inf, math.inf, True)
         return move
 
 
     def minimax(self, board, depth, alpha, beta, maximizing_player):
-        pass
-
-        #if depth == 0 or board.is_winner() or board.is_board_full():
-        if depth == 0:
+        
+        if depth == 0 or SeegaRules.is_end_game(board):
             return None, self.evaluate(board)
 
-        children = SeegaRules.get_player_all_cases_actions(board, self.position)
+        children = SeegaRules.get_player_all_cases_actions(board, self.position)        
         best_move = children[0]
         
         if maximizing_player:
             max_eval = -math.inf        
             for child in children:
                 board_copy = board
-                pprint(vars(board_copy))
-                pprint(board_copy)
                 board_copy, is_the_end = SeegaRules.make_move(board_copy, child, self.position)
-                pprint(board_copy)
-                current_eval = self.minimax(board_copy, depth - 1, alpha, beta, False)[1]
+                move = child
+                current_eval = 0
+                move, current_eval = self.minimax(board_copy, depth - 1, alpha, beta, False)
                 if current_eval > max_eval:
                     max_eval = current_eval
                     best_move = child
@@ -88,7 +88,9 @@ class AI(Player):
             for child in children:
                 board_copy = board
                 board_copy, is_the_end = SeegaRules.make_move(board_copy, child, self.position)
-                current_eval = self.minimax(board_copy, depth - 1, alpha, beta, True)[1]
+                move = child
+                current_eval = 0
+                move, current_eval = self.minimax(board_copy, depth - 1, alpha, beta, True)
                 if current_eval < min_eval:
                     min_eval = current_eval
                     best_move = child
@@ -99,7 +101,6 @@ class AI(Player):
 
     def evaluate(self, board):
         if board.captured is not None:
-            pprint('CAPTURED')
-            return -1
-        else :
             return 1
+        else :
+            return -1
